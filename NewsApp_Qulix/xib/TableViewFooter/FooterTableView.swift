ba0a86444ef1fc5f8d721d2516dc3888d8ca8f68
Andrei <atrakhimovich95@gmail.com>
2021-08-26 12:36:34 +0300

    version 2.0.1

--- NewsApp_Qulix/xib/TableViewFooter/FooterTableView.swift
+++ NewsApp_Qulix/xib/TableViewFooter/FooterTableView.swift
@@ -10,21 +10,21 @@ import UIKit
 class FooterTableView: UITableViewHeaderFooterView {
 
     let activityIndicator = UIActivityIndicatorView()
-    
+
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
         configureContents()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     func configureContents() {
         activityIndicator.translatesAutoresizingMaskIntoConstraints = false
-    
+
         contentView.addSubview(activityIndicator)
-        
+
         NSLayoutConstraint.activate([
             activityIndicator.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             activityIndicator.centerYAnchor.constraint(equalTo: self.centerYAnchor)
